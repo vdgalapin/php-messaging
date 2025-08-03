398f45d6736f765d0de9c419154522c1686de637
Vyron Galapin <vdgalapin@gmail.com>
2025-08-03 13:43:45 -0700

    Fix the database configuration

---
+++ config/db.sample.php
@@ -0,0 +1,14 @@
+<?php
+$host = getenv('DB_HOST');
+$db = getenv('DB_NAME');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
+$port = getenv('DB_PORT') ?: 5432;
+
+try {
+    $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$db", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database connection failed.");
+}
+?>
