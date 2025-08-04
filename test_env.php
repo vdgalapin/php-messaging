59c5cf0123d9a1a1c30aaf056d04cd75fbed9494
Vyron Galapin <vdgalapin@gmail.com>
2025-08-03 19:51:02 -0700

    check environment

---
+++ test_env.php
@@ -0,0 +1,9 @@
+<?php
+echo "<pre>";
+echo "DB_HOST: " . getenv('DB_HOST') . "\n";
+echo "DB_NAME: " . getenv('DB_NAME') . "\n";
+echo "DB_USER: " . getenv('DB_USER') . "\n";
+echo "DB_PASS: " . getenv('DB_PASS') . "\n";
+echo "DB_PORT: " . getenv('DB_PORT') . "\n";
+echo "</pre>";
+?>
