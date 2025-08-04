39a820f4b0abb86ccd98b50c3bd8b91094728596
Vyron Galapin <vdgalapin@gmail.com>
2025-08-03 18:12:17 -0700

    Fix typo error

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <?php endif; ?>
 
 <h2>Login</h2>
-<form method="POST" action="auth/Login.php">
+<form method="POST" action="auth/login.php">
     <input type="text" name="username" placeholder="Username" reqruired><br>
     <input type="password" name="password" placeholder="Password" required><br>
     <button type="submit">Login</button>
